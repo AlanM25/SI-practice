c686f12c6a4645eefb394f5355a6dd0c0f129360
AlanM25 <2230233@upv.edu.mx>
2025-03-07 12:45:07 -0600

    clase 07/03

--- Practicas en clase/softmax.py
+++ Practicas en clase/softmax.py
@@ -1,8 +1,10 @@
 from sklearn.datasets import make_blobs
 import numpy as np
 
-n_samples = 300
-Lista = [[2, 6], [6, 2], [8, 4], [0, 0]]
+n_samples = 500
+#Lista = [[2, 6], [6, 2], [8, 4], [0, 0]]
+#Lista = [[6, 2], [8, 4], [0, 0]]
+Lista = [[0, 0], [0, 5], [5, 0], [5, 5], [10, 5], [10, 10]]
 samples, labels = make_blobs(n_samples=n_samples,
                              centers=(Lista),
                              random_state=0)
@@ -25,31 +27,34 @@ test_data = samples[-size_of_test_sample:]
 learn_labels = labels[:size_of_learn_sample]
 test_labels = labels[-size_of_test_sample:]
 
-from neural_network_softmax import NeuralNetwork
-
-simple_network = NeuralNetwork(no_of_in_nodes=2, 
-                               no_of_out_nodes=2, 
-                               no_of_hidden_nodes=5,
-                               learning_rate=0.3,
-                               softmax=False)
-
-#for x in [(1, 4), (2, 6), (3, 3), (6, 2)]:
-#    y = simple_network.run(x)
-#    print(x, y, s.sum())
-
-#labels_one_hot = (np.arange(2) == labels.reshape(labels.size, 1))
-labels_one_hot = (np.arange(2) == learn_labels.reshape(size_of_learn_sample, 1))
+# labels_one_hot = (np.arange(2) == labels.reshape(labels.size, 1))
+labels_one_hot = (np.arange(NumeroClases) == learn_labels.reshape(size_of_learn_sample, 1))
 labels_one_hot = labels_one_hot.astype(np.float64)
+#print(labels_one_hot)
 
-for i in range(size_of_learn_sample):
-    # print(learn_data[i], labels[i], labels_one_hot[i])
-    simple_network.train(learn_data[i],
-                         labels_one_hot[i])
+from neural_network_softmax import NeuralNetwork
+LX = [True, False]
+for e in LX:
+    simple_network = NeuralNetwork(no_of_in_nodes=2,
+                                   no_of_out_nodes=NumeroClases,
+                                   no_of_hidden_nodes=5,
+                                   learning_rate=0.3,
+                                   softmax=e)
 
-from collections import Counter
-evaluation = Counter()
-print(len(learn_data))
-print(len(test_data))
+    # for x in [(1, 4), (2, 6), (3, 3), (6, 2)]:
+    #    y = simple_network.run(x)
+    #    print(x, y, s.sum())
 
-print(simple_network.evaluate(learn_data, learn_labels))
-print(simple_network.evaluate(test_data, test_labels))
+    for i in range(size_of_learn_sample):
+        # print(learn_data[i], labels[i], labels_one_hot[i])
+        simple_network.train(learn_data[i],
+                             labels_one_hot[i])
+
+    from collections import Counter
+
+    evaluation = Counter()
+    #print(len(learn_data))
+    #print(len(test_data))
+
+    print(simple_network.evaluate(learn_data, learn_labels))
+    print(simple_network.evaluate(test_data, test_labels))
